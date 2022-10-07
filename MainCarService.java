50aaf2e16745dd7f56fe77ceb2f9ab153a010478
Haura Adelia Melvandri <102033602+hauraaadeliamelvandri@users.noreply.github.com>
2022-10-07 23:14:23 +0700

    Add files via upload

---
+++ MainCarService.java
@@ -0,0 +1,34 @@
+class MainCarService {
+   public static void main(String [] args) {
+      AbadiSelalu data = new AbadiSelalu();
+
+      // clear screen console
+      System.out.print('\u000C');
+      System.out.println("--- Car Cleaning... Anda Pesan Kami Datang ---");
+      System.out.println("============================================");
+      System.out.println("");
+      System.out.println("");
+
+      // render content scanner
+      data.dataPeserta();
+      System.out.print("\n");
+      data.jenisJasa();
+      System.out.print("\n");
+      data.teamPenyediaJasa();
+      System.out.print("\n");
+      data.keteranganPemesanan();
+      System.out.print("\n");
+      data.biayaPenyediaJasa();
+
+      System.out.println("");
+      System.out.println("");
+      System.out.println("=======================================");
+      System.out.println("Jasa yang di Pesan: " + data.tampilkanJenisJasa());
+      System.out.println("Nama Penyedia Jasa: " + data.tampilkanPenyediaJasa());
+      System.out.println("Keterangan: " + data.tampilkanKeterangan());
+      System.out.println("");
+      System.out.println("Total Biaya yang di Bayar Pemesanan Rp. " + data.jumlahkanBiayaJasa());
+      System.out.println("");
+      System.out.println("Salam Bersih " + data.namaPeserta() + " Terimakasih");
+   }
+}
